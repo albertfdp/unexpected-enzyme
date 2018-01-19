b336bd6167424ac627cfd9f6d2cd5707d06d7b1a
Sune Simonsen <sune@we-knowhow.dk>
2018-01-19 10:25:10 +0100

    Inspect ReactWrapper correctly

--- src/types/ReactWrapper.js
+++ src/types/ReactWrapper.js
@@ -2,9 +2,11 @@ import ReactWrapper from 'enzyme/build/ReactWrapper';
 
 const ReactWrapperType = {
   name: 'ReactWrapper',
-  identify: reactWrapper => reactWrapper instanceof ReactWrapper,
-  inspect: (reactWrapper, depth, output, inspect) => {
-    output.text(`ReactWrapper`);
+  identify: function(reactWrapper) {
+    return reactWrapper && reactWrapper instanceof ReactWrapper;
+  },
+  inspect: function(reactWrapper, depth, output) {
+    output.appendInspected(reactWrapper.getElement());
   }
 };
 
