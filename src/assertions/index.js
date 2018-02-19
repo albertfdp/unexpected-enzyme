706b0b68592d7e2cb49baf0bd51209b84eb0ed24
Albert Fernández <albert@nandez.cat>
2018-02-19 18:02:40 +0100

    Inline assertions & types

--- src/assertions/index.js
+++
@@ -1,9 +0,0 @@
-import fullRenderAssertions from './fullRenderAssertions';
-
-const assertions = {
-  installInto: function(childExpect) {
-    fullRenderAssertions(childExpect);
-  }
-};
-
-export default assertions;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import assertions from './assertions';
-import types from './types';
 import unexpectedReact from 'unexpected-react';
+import React from 'react';
+import ReactWrapper from 'enzyme/build/ReactWrapper';
 
 const unexpectedEnzyme = {
   name: 'unexpected-enzyme',
@@ -8,8 +8,74 @@ const unexpectedEnzyme = {
   installInto: function(expect) {
     var childExpect = expect.child();
     childExpect.use(unexpectedReact);
-    types.installInto(childExpect);
-    assertions.installInto(childExpect);
+
+    childExpect.exportType(childExpect.getType('ReactElement'));
+
+    childExpect.exportType({
+      name: 'ReactWrapper',
+      identify: function(reactWrapper) {
+        return reactWrapper && reactWrapper instanceof ReactWrapper;
+      },
+      inspect: function(reactWrapper, depth, output) {
+        output.appendInspected(reactWrapper.getElement());
+      }
+    });
+
+    childExpect.exportAssertion(
+      '<ReactWrapper> [not] to be checked',
+      (expect, reactWrapper) => {
+        expect(reactWrapper.instance().checked, '[not] to be true');
+      }
+    );
+
+    childExpect.exportAssertion(
+      '<ReactWrapper> [not] to contain <ReactElement>',
+      (expect, reactWrapper, reactElement) => {
+        if (
+          expect.flags.not ===
+          reactWrapper.containsMatchingElement(reactElement)
+        ) {
+          expect.errorMode = 'bubble';
+
+          return expect(
+            reactWrapper.instance(),
+            '[not] to contain with all wrappers',
+            reactElement
+          );
+        }
+      }
+    );
+
+    childExpect.exportAssertion(
+      '<ReactWrapper> [not] to have props satisfying <object>',
+      (expect, reactWrapper, props) => {
+        return expect(reactWrapper.props(), '[not] to satisfy', props);
+      }
+    );
+
+    childExpect.exportAssertion(
+      '<ReactWrapper> when setting props <object> <assertion>',
+      (expect, reactWrapper, props) => {
+        return expect.shift(reactWrapper.setProps(props));
+      }
+    );
+
+    childExpect.exportAssertion(
+      '<ReactWrapper> when receiving event <string>',
+      (expect, reactWrapper, event) => {
+        return expect.shift(reactWrapper.simulate(event));
+      }
+    );
+
+    childExpect.exportAssertion(
+      '<ReactWrapper> [not] to have class <string>',
+      (expect, reactWrapper, className) => {
+        if (expect.flags.not === reactWrapper.hasClass(className)) {
+          const classes = (reactWrapper.prop('className') || '').split(' ');
+          expect(classes, '[not] to contain', className);
+        }
+      }
+    );
   }
 };
 
